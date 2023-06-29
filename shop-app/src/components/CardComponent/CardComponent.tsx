052bcae0e7f20125ae819a4bef9197c5e6e66d72
huyshine <huyshine1111@gmail.com>
2023-06-29 13:46:39 +0700

    Build CardComponent

---
+++ shop-app/src/components/CardComponent/CardComponent.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { Card, Image } from "antd";
+import { StyleNameProduct, WrapperCardStyle, WrapperDiscountText, WrapperPriceText, WrapperReportText } from "./style";
+import { StarFilled } from "@ant-design/icons";
+
+const { Meta } = Card;
+
+const CardComponent = () => {
+  return (
+    <WrapperCardStyle
+      hoverable
+      headStyle={{width: '200px' , height: '200px'}}
+      style={{ width: 200 }}
+      cover={ 
+        <img
+          alt="example"
+          src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png"
+        />
+      }
+    >
+      <img src="https://thumbs.dreamstime.com/b/great-deals-red-rubber-stamp-over-white-background-88004036.jpg" style={{width: '68px' , height: '20px' , position: 'absolute' , top: '-1px' , left: '-1px', borderTopLeftRadius: '3px'}}/>
+      <StyleNameProduct>Iproduct</StyleNameProduct>
+      <WrapperReportText>
+        <span>4.96<StarFilled style={{ fontSize: "10px", color: "yellow" }} /></span>
+        <span>| Đã bán 1000+</span>
+      </WrapperReportText>
+      <WrapperPriceText>
+        1.000.000đ
+        <WrapperDiscountText>
+            -5%
+        </WrapperDiscountText>
+      </WrapperPriceText>
+    </WrapperCardStyle>
+  );
+};
+
+export default CardComponent;
--- shop-app/src/pages/HomePage/HomePage.tsx
+++ shop-app/src/pages/HomePage/HomePage.tsx
@@ -1,25 +1,48 @@
-import SliderComponent from "../../components/SliderComponent/SliderComponent"
-import TypeProduct from "../../components/TypeProduct/TypeProduct"
-import { WrapperTypeProduct } from "./style"
+import CardComponent from "../../components/CardComponent/CardComponent";
+import SliderComponent from "../../components/SliderComponent/SliderComponent";
+import TypeProduct from "../../components/TypeProduct/TypeProduct";
+import { WrapperTypeProduct } from "./style";
 
 export const HomePage = () => {
-  const arr = [ 'TV' , 'Tu Lanh' , 'Lap top']
-  const slider1 = 'https://res.cloudinary.com/do9rcgv5s/image/upload/v1669080502/bia_1_jkrk1w.webp'
-  const slider2 = 'https://res.cloudinary.com/do9rcgv5s/image/upload/v1669080502/bia_1_jkrk1w.webp'
-  const slider3 = 'https://res.cloudinary.com/do9rcgv5s/image/upload/v1669080502/bia_1_jkrk1w.webp'
-  const slider = [ slider1 , slider2 , slider3 ]
-  
+  const arr = ["TV", "Tu Lanh", "Lap top"];
+  const slider1 =
+    "https://res.cloudinary.com/do9rcgv5s/image/upload/v1669080502/bia_1_jkrk1w.webp";
+  const slider2 =
+    "https://res.cloudinary.com/do9rcgv5s/image/upload/v1669080502/bia_1_jkrk1w.webp";
+  const slider3 =
+    "https://res.cloudinary.com/do9rcgv5s/image/upload/v1669080502/bia_1_jkrk1w.webp";
+  const slider = [slider1, slider2, slider3];
+
   return (
-    <div style={{padding : ' 0 120px'}}>
-      <WrapperTypeProduct>
-      {arr.map((item)=> {
-        return (
-          <TypeProduct name={item} key={item}/>
-        )
-      })}
-      {/* HomePage */}
-    </WrapperTypeProduct>
-    <SliderComponent arrImages={{slider}}/>
-    </div>
-  )
-}
+    <>
+      <div style={{ padding: " 0 120px" }}>
+        <WrapperTypeProduct>
+          {arr.map((item) => {
+            return <TypeProduct name={item} key={item} />;
+          })}
+          {/* HomePage */}
+        </WrapperTypeProduct>
+      </div>
+      <div
+        id="container"
+        style={{
+          backgroundColor: "#effef",
+          padding: "0 120px",
+          height: "100px",
+        }}
+      >
+        <SliderComponent arrImages={{ slider }} />
+        <div
+          style={{
+            marginTop: "20px",
+            display: "flex",
+            alignItems: "center",
+            gap: "20px",
+          }}
+        >
+          <CardComponent />
+        </div>
+      </div>
+    </>
+  );
+};
